0a46853bc9ee867c80ba4b0828dfb70a0131c45f
Mohammad Kfelati <mohammad.kfelati>
2025-04-03 00:59:14 +0300

    fix terminal

--- src/social-accounts/social-accounts.module.ts
+++ src/social-accounts/social-accounts.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SocialAccountsService } from './social-accounts.service';
 import { SocialAccountsController } from './social-accounts.controller';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [SocialAccountsController],
-  providers: [SocialAccountsService, PrismaService],
+  providers: [SocialAccountsService],
 })
 export class SocialAccountsModule {}
--- src/subscription-plan/subscription-plan.module.ts
+++ src/subscription-plan/subscription-plan.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SubscriptionPlanController } from './subscription-plan.controller';
 import { SubscriptionPlanService } from './subscription-plan.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [SubscriptionPlanController],
-  providers: [SubscriptionPlanService]
+  providers: [SubscriptionPlanService],
 })
 export class SubscriptionPlanModule {}
